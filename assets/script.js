7f33941be28d3aa60f9f939d013f9c97ebe04e96
Fati Haruna <haruna.fati@gmail.com>
2022-04-04 02:58:35 -0400

    fixing js function

--- assets/script.js
+++ assets/script.js
@@ -4,16 +4,32 @@ function getInfo() {
     const newName= document.getElementById("cityInput");
     const cityName= document.getElementById("cityName");
     cityName.innerHTML = "--"+newName.value+"--"
-}
+    
+
+const button = document.querySelector(".search");
+button.addEventListener("click"), function () {
 
 fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=962785f671489b57dbf66ef34008daec")
-.then(response => response.json())
+.then((response) => response.json())
+    .then ((response) => {
+    console.log(response);
+})
 
 // icons display by weather
 .then(data =>{
+   console.log(data); 
     for(i=0;i<5; i++){
         document.getElementById("img"+ (i+1)).src ="http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
     }
 })
+};
+}
+// array for each day 
+const d =new Date();
+const weekday =["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
-// array for each day function
+function checkDay(day){
+for (i=0;i<5;i++){
+    document.getElementById("day"+(i+1)).innerHTML = weekday[checkDay]
+}
+}
