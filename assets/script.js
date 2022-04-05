7c7e6e0a10ea97a972ff9e1029fbeb44489877a5
Fati Haruna <haruna.fati@gmail.com>
2022-04-05 01:25:32 -0400

    changing app due to functionality

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,22 @@
-//to change the name of city when it is input
+// For weather information
 
-function getInfo() {
-    const newName= document.getElementById("cityInput");
-    const cityName= document.getElementById("cityName");
-    cityName.innerHTML = "--"+newName.value+"--"
-    
+const cityEl = $('h2#city');
+    const dateEl = $('h3#date');
+    const weatherIconEl = $('img#weather-icon');
+    const temperatureEl = $('span#temperature');
+    const humidityEl = $('span#humidity');
+    const windEl = $('span#wind');
+    const uvIndexEl = $('span#uv-index');
+    const cityListEl = $('div.cityList');
 
-const button = document.querySelector(".search");
-button.addEventListener("click"), function () {
+    const cityInput = $('#city-input');
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=962785f671489b57dbf66ef34008daec")
-.then((response) => response.json())
-    .then ((response) => {
-    console.log(response);
-})
-
-// icons display by weather
-.then(data =>{
-   console.log(data); 
-    for(i=0;i<5; i++){
-        document.getElementById("img"+ (i+1)).src ="http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
+    function buildURLFromInputs(city) {
+        if (city) {
+            return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        }
     }
-})
-};
-}
-// array for each day 
-const d =new Date();
-const weekday =["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
-function checkDay(day){
-for (i=0;i<5;i++){
-    document.getElementById("day"+(i+1)).innerHTML = weekday[checkDay]
-}
-}
+    function buildURLFromId(id) {
+        return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`;
+    }
